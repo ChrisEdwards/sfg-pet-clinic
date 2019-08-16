915314a0d8adb542277c68814714e9d0dfaf7df0
Chris Edwards <chris.edwards@kasasa.com>
2019-08-15 23:10:31 -0500

    Created Spring Data JPA service for Pets

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/PetTypeServiceJpa.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/PetTypeServiceJpa.java
@@ -3,12 +3,14 @@ package guru.springframework.sfgpetclinic.services.jpa;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Profile("jpa")
 public class PetTypeServiceJpa implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;
